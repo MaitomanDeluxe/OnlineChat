8c5e01b230da9ca642dfbbdd3ae73bf25a6b5e79
MaitomanDeluxe <maikanamaikana@outlook.jp>
2025-07-08 22:35:14 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const ws = new WebSocket("wss://superchat.maikanamaikana.workers.dev/chat/room1");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const messages = document.getElementById("messages");
+
+ws.onmessage = (event) => {
+  const div = document.createElement("div");
+  div.textContent = event.data;
+  messages.appendChild(div);
+  messages.scrollTop = messages.scrollHeight;
+};
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  if (input.value.trim()) {
+    ws.send(input.value);
+    input.value = "";
+  }
+};
